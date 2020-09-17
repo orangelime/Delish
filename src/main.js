f4c6330c79d07aa0d391d324b8457f20ccdf22e4
orangelime <gobbiby@gmail.com>
2020-09-17 16:02:39 +0800

    sass package

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/sass/app.scss';
-import '@/assets/css/icon-font.css';
+//import '@/assets/sass/app.scss';
+import '@/assets/css/style.css';
+//import '@/assets/css/icon-font.css';
 
 
 
