3d8b918075a8d698a3c6ed49c5a908b00538279e
orangelime <gobbiby@gmail.com>
2021-06-25 13:54:19 +0800

    修改axios.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,36 +1,32 @@
 import axios from 'axios'
 import store from "@/store";
-import router from '@/router';
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
-// const token = localStorage.getItem('token');
+// axios.interceptors.response.use((response) => {
+//     return response
+// }, (error) => {
+//     //console.log(error.response.data)
+// if (error.response.status === 401) {
+//     store.commit('user',null);
+//     localStorage.removeItem("token");
+// }
+//     return Promise.reject(error)
+// })
 
-// axios.defaults.headers.common['Authorization'] = `Bearer ${token}`; 
-axios.interceptors.response.use((response) => {
-    return response
-}, (error) => {
-    //console.log(error.response.data)
-if (error.response.status === 401) {
-    store.commit('user',null);
-    localStorage.removeItem("token");
-}
-    return Promise.reject(error)
-})
-
-// axios.interceptors.response.use(
-//     response => response,
-//     error => {
-//         if (error.response.status === 422) {
-//             store.commit("setErrors", error.response.data.errors);
-//         } else if (error.response.status === 401) {
-//             store.commit('user', null);
-//             localStorage.removeItem("token");
-//         } else {
-//             return Promise.reject(error);
-//         }
-//     }
-// );
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        if (error.response.status === 422) {
+            store.commit("setErrors", error.response.data.errors);
+        } else if (error.response.status === 401) {
+            store.commit('user', null);
+            localStorage.removeItem("token");
+        } else {
+            return Promise.reject(error);
+        }
+    }
+);
 
 axios.interceptors.request.use((config) =>{
     config.headers.common = {
