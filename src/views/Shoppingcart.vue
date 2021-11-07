fd0b3bdea4ab313c4756f9a1d5de2578b7a58cc8
orangelime <gobbiby@gmail.com>
2021-11-08 01:20:34 +0800

    update shoppingcart mealsCart data and some typograph

--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -4,18 +4,45 @@
         <CommonHeader :title="title"></CommonHeader>
         
         <!-- select menu -->
-        <div class="row">
+        <!-- <div class="row"> -->
 
             <!--reservation-->
-            <div class="shoppingcart__content shoppingcart__content--right">
-                <div class="shoppingcart__basket">
-                    
-                
-                    
-                </div>
+            <div class="shoppingcart__content">
+                <div class="shoppingcart__box">
+                    <table class="shoppingcart__table">
+                        <thead>
+                            <tr>
+                                <th colspan="5">Order</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in mealsCart" :key="item.index">
+                                <div v-for="meal in item.mealDetails" :key="meal.idMeal">
+                                    <td>
+                                        <img class="popup__logo" :src=meal.strMealThumb :alt=meal.strMeal>
+                                    </td>
+                                    <td>
+                                        <div class="u-center-text">
+                                            <h3 class="heading-secondary">
+                                                {{meal.strMeal}}
+                                            </h3>
+                                        </div>
+                                    </td>
+                                </div>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td></td>
+                                <td colspan="3">Total <span class="total-price">$888</span></td>
+                                <td></td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                    <button type="submit" class="btn btn--signin btn--signin-1">Book it!</button>
+                </div>    
             </div>
-            
-        </div>
+        <!-- </div> -->
     </div>
 </template>
 <script>
@@ -44,22 +71,25 @@ export default {
         CommonHeader,
     },
     methods:{
-        // ...mapActions(['signIn','signOut']),
-        // userSignOut(){
-        //     this.$store.dispatch('logout');
-        //     localStorage.removeItem('token');
-        //     // this.$store.dispatch('user',null);  laravel後臺登入
-        //     this.$router.push('/index');
-        //     this.$store.dispatch('gSignin/signOut');
-        // }
+        
     },
     computed:{
-        // ...mapState({
-        //     signedIn: state => state.gSignin.signedIn,
-        //     profile: state => state.gSignin.profile
-        // }),
-        // // ...mapGetters(['user','menus','menuList'])  laravel後臺登入
-        // ...mapGetters(['isSignIn','menus','menuList'])
+        ...mapGetters(['mealsCart']),
+        mealsInCart(){
+            // let arr = this.mealsCart;
+            // //console.log(arr.mealDetails)
+            // for(let i=0;i<arr.length;i++){
+            //     return arr[i].mealDetails;
+            // }
+
+            let dict = {};
+            this.mealsCart.forEach(item => {
+                dict[item.idMeal] = item;
+            })
+            return dict;
+
+            
+        }
     }
 }
 </script>
